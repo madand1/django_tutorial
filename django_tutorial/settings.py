0c67987bd0305aa422e14edbc986c30aae745f3c
mad.and1 <asirandyglez@gmail.com>
2025-03-25 11:31:50 +0000

    Nombre

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '9f0h)gozf$g%6igo8&767w1xro0adm+)msxe)!eic$!fhvynb8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
