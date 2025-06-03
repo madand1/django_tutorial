f942d8b6e415fe989c67432e359928fcc7642fa4
madand1 <asirandy@gmail.com>
2025-06-03 13:18:19 +0200

    Cambio a return True

--- polls/models.py
+++ polls/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
 
     def was_published_recently(self):
         now = timezone.now()
-        return true
+        return True
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
